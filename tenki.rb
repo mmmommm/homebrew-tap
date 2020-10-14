099f1f5ed447d3a0b12bbe0e52b937e4438c65e6
ryota kise <ryota1839@gmail.com>
2020-10-14 11:50:01 +0900

    Update tenki.rb

--- tenki.rb
+++ tenki.rb
@@ -5,7 +5,7 @@ class Tenki < Formula
   homepage "https://github.com/mmmommm/tenki"
   url “https://github.com/mmmommm/tenki/releases/download/v1.0.6/tenki_1.0.6_darwin_amd64.tar.gz”
   sha256 “0c44b5694504e42406d7ad77c05c39a2c7414ba4302c8edd5ec008862e44e47e”
-  license “”
+  depends_on 'go' => :build
 
   # depends_on "cmake" => :build
 
